6c442b0a17bece3a6f327998bda727d71173ff51
unknown <you@example.com>
2020-10-04 18:05:34 +0330

    send email verify

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,9 @@ Route::fallback(function () {
     return view('home');
 });
 
-//Auth::routes();
+//Auth::routes(['verify' => true]);
 Route::post('/login', 'Auth\LoginController@login');
 Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 Route::post('/register', 'Auth\RegisterController@register')->name('register');
+Route::post('/email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
